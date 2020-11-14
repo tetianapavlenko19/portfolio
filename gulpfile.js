400941bdcdc028af6d93da3cf1ae3dee0f20a75c
tetianapavlenko19 <tmj@ukr.net>
2020-11-14 21:14:58 +0300

    hero-block

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('./sass/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
